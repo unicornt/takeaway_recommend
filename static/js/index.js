a3d8caaa87bbceaa44f9423c1c3047e69b07c9bf
unicornt <unicorntwin@outlook.com>
2021-05-07 15:05:06 +0800

    add like button & sort button

--- static/js/index.js
+++ static/js/index.js
@@ -53,5 +53,21 @@ $(document).ready(function () {
             '</ul>';
     }
     $("#navbarSupportedContent").append(doc);
-});
 
+    var $win = $(window);
+    var $backToTop = $('.js-back-to-top');
+    // 当用户滚动到离顶部100像素时，展示回到顶部按钮
+    $win.scroll(function() {
+        if ($win.scrollTop() > 100) {
+            $backToTop.show();
+        } else {
+            $backToTop.hide();
+        }
+    });
+    // 当用户点击按钮时，通过动画效果返回头部
+    $backToTop.click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 200);
+    });
+});
